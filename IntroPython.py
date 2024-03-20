31bb1f9715c1500ae8042d27dde880964bb74d57
Mariana Arango <marangob@udistrital.edu.co>
2024-03-20 15:23:00 -0500

    Operaciones

--- IntroPython.py
+++ IntroPython.py
@@ -1,4 +1,41 @@
-#Esto es un comentario de una línea 
-#Todo lo que va después es ignorado
-#por el interprete de python 
+# Esto es un comentario de una línea 
+# Todo lo que va después es ignorado
+# por el interprete de python 
 
+# Variables: Espacio de memoria, con nmbre, donde guardo valores
+# Los nombres de variables deben se cortos, descriptivos, NO TENER ESPACIOS
+# EN BLANCO ni caracteres especiales, no deben empezar por número
+
+# Descriptivo significa que representa la categoría del dato que quiero guardar
+# números (enteros, reales), cadenas de carácteres (string), booleanos (True, False)
+# caracteres.
+
+variable = 1
+
+variable = 'Hola'
+
+variable = True
+
+variable = 'a'
+
+variable = 3.14159265365
+
+# Para asignar un valor a la variable se una el operador =
+
+
+
+# Operadores: Mecanismo para obtener un dato a partir de otros datos.
+# Los datos que intervienen se llaman operadores 
+
+# Aritmeticos: + - / %
+# De comparación: Retornan True or False. < > >= <= == !=
+# Los de lógica boolana: AND OR. Retornan True o False de a cuerdo a una 
+# tabla de verdad. Los operadores siempre son booleanos (True or False)
+
+a = True
+b = False 
+
+print (a and b)
+
+# Los operadores booleanos y de comparación son muy utilizados al
+# definir condiciones 
