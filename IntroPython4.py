83920b6f69a03a3d1350353651e915082829d8fb
Mariana Arango <marangob@udistrital.edu.co>
2024-04-02 14:52:01 -0500

    prueba 2/4/2024

---
+++ IntroPython4.py
@@ -0,0 +1,13 @@
+# PROMPT: Pregunta 
+# 1. Claros
+# 2. Concisos
+# 3. Evitar la ambiguedad 
+# 4. Debe ir al punto 
+# 5. Iterativos
+
+# Decomposición de problemas 
+# Abstractos (Oculten complejidad)
+# Pensamiento algoritmico 
+# Reconocwer patrones 
+
+#Funciones recursivas: Funciones que se llaman a sí mismas 
---
+++ Taller_3.py
@@ -0,0 +1,2 @@
+from IngresarNumero import 
+
